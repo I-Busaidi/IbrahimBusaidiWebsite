95de0281045ae0ff447c7a45e1d85f44ac98646b
Ibrahim Al Busaidi <i.s.busaidi@gmail.com>
2026-01-11 16:18:26 +0400

    added User & Profile models.

---
+++ IbrahimBusaidiWebsite/Data/Users/Models/Profile.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace IbrahimBusaidiWebsite.Data.Users.Models
+{
+    public class Profile
+    {
+        // Shared PK + FK
+        [Key]
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; } = default!;
+        // one-to-one relation between User & UserProfile
+        public User User { get; set; } = default!;
+
+        public string? DisplayName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string? PreferredLanguage { get; set; }
+        public string? Nationality { get; set; }
+        public string? TimeZone { get; set; }
+    }
+}
---
+++ IbrahimBusaidiWebsite/Data/Users/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace IbrahimBusaidiWebsite.Data.Users.Models
+{
+    // inherits the user model from AspNetCore Identity package
+    public class User : IdentityUser
+    {
+        // Adding a User Profile for each user
+        public Profile? Profile { get; set; }
+    }
+}
