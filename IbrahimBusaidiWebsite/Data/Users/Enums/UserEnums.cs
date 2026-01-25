3dd66911267a724ff772a5b10d696f86e5aed574
Ibrahim Al Busaidi <i.s.busaidi@gmail.com>
2026-01-25 16:11:51 +0400

    Add User entity related enums.

---
+++ IbrahimBusaidiWebsite/Data/Users/Enums/UserEnums.cs
@@ -0,0 +1,25 @@
+﻿namespace IbrahimBusaidiWebsite.Data.Users.Enums
+{
+    public class UserEnums
+    {
+        public enum AddressType
+        {
+            Shipping = 0,
+            Billing = 1
+        }
+
+        public enum PrefLanguage
+        {
+            English = 0,
+            Arabic = 1
+        }
+
+        public enum MfaMethod
+        {
+            None = 0,
+            Email = 1,
+            Sms = 2,
+            AuthApp = 3
+        }
+    }
+}
