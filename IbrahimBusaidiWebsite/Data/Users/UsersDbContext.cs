e24d4deb7f9f293c88806eb851578b095e3931b9
Ibrahim Al Busaidi <i.s.busaidi@gmail.com>
2026-01-25 16:13:28 +0400

    Added User DB context.

---
+++ IbrahimBusaidiWebsite/Data/Users/UsersDbContext.cs
@@ -0,0 +1,25 @@
+﻿using IbrahimBusaidiWebsite.Data.Users.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IbrahimBusaidiWebsite.Data.Users
+{
+    public class UsersDbContext : IdentityDbContext<User, IdentityRole, string>
+    {
+        public UsersDbContext(DbContextOptions<UsersDbContext> options) : base (options){ }
+
+        public DbSet<Profile> Profiles => Set<Profile>();
+        public DbSet<Settings> Settings => Set<Settings>();
+        public DbSet<Address> Addresses => Set<Address>();
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Adding user related tables under the same schema for better organization
+            builder.HasDefaultSchema("Identity");
+
+            builder.Entity<Address>().HasIndex(a => a.UserId);
+        }
+    }
+}
