abaff99d694f98e016664c687bd8431558594050
Ibrahim Al Busaidi <i.s.busaidi@gmail.com>
2026-01-27 14:27:18 +0400

    Modified User Db Context to include different schemas for profiles, addresses, and settings. Changed Tables names to avoid confusion.

--- IbrahimBusaidiWebsite/Data/Users/UsersDbContext.cs
+++ IbrahimBusaidiWebsite/Data/Users/UsersDbContext.cs
@@ -9,15 +9,21 @@ namespace IbrahimBusaidiWebsite.Data.Users
     {
         public UsersDbContext(DbContextOptions<UsersDbContext> options) : base (options){ }
 
-        public DbSet<Profile> Profiles => Set<Profile>();
-        public DbSet<Settings> Settings => Set<Settings>();
+        public DbSet<Profile> UserProfiles => Set<Profile>();
+        public DbSet<Settings> UserSettings => Set<Settings>();
         public DbSet<Address> Addresses => Set<Address>();
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
 
             // Adding user related tables under the same schema for better organization
-            builder.HasDefaultSchema("Identity");
+            builder.HasDefaultSchema("identity");
+
+            // Explicitly adding profile, settings, and addresses tables to other schemas
+            builder.Entity<Address>().ToTable("Addresses", "profiles");
+            builder.Entity<Profile>().ToTable("UserProfiles", "profiles");
+
+            builder.Entity<Settings>().ToTable("UserSettings", "settings");
 
             builder.Entity<Address>().HasIndex(a => a.UserId);
         }
